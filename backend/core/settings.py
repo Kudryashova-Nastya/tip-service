670713fbb1b623521229c8cbf9f93a284d980641
zakhep66 <sur.mikhael@gmail.com>
2022-06-15 19:01:31 +0300

    починил ошибку при получении токена

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = dotenv.load_dotenv('SECREY_KEY')
+SECRET_KEY = str(dotenv.load_dotenv('SECREY_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
