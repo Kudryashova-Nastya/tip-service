39429afc9bc2776ddc1b41d9e8b9a954962628c1
zakhep66 <sur.mikhael@gmail.com>
2022-06-16 18:42:48 +0300

    перенёс срок жизни токенов в settings

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -155,8 +155,8 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=int(dotenv.load_dotenv('ACCESS_TOKEN_LIFETIME'))),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=int(dotenv.load_dotenv('REFRESH_TOKEN_LIFETIME'))),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': False,
     'UPDATE_LAST_LOGIN': False,
@@ -182,6 +182,6 @@ SIMPLE_JWT = {
     'JTI_CLAIM': 'jti',
 
     'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
-    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=int(dotenv.load_dotenv('ACCESS_TOKEN_LIFETIME'))),
-    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=int(dotenv.load_dotenv('REFRESH_TOKEN_LIFETIME'))),
+    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=15),
+    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
 }
