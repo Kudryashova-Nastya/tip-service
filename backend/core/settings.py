a2707c32a448f585dea47d60306de86b754ce43c
zakhep66 <sur.mikhael@gmail.com>
2022-06-16 20:36:39 +0300

    убрал cors

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -150,9 +150,9 @@ REST_FRAMEWORK = {
         ),
 }
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000",
+# ]
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
